ca6205dd486f4eb6ce6011399a433d68e2cde522
ErnTrgt <erntrgt10@gmail.com>
2024-04-11 03:06:55 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
+
 function App() {
-    return <div>hi</div>;
+    return (
+        <div
+            style={{
+                maxWidth: "800px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+                margin: "0 auto",
+                alignItems: "center",
+            }}
+        >
+            <div style={{ fontSize: "2rem", textAlign: "center" }}>
+                {"Winner! "}
+                {"Nice Try "}
+            </div>
+
+            <HangmanDrawing />
+            <HangmanWord />
+            <Keyboard />
+        </div>
+    );
 }
 
 export default App;
